4dcb49e9ab524f1d89d425bc68a4fdbd7fb83a6b
Alejandro Saucedo <axsauze@gmail.com>
2021-06-14 16:28:30 +0100

    Updated docs to consistent theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,3 +1,4 @@
+import sphinx_material
 # -*- coding: utf-8 -*-
 #
 # Configuration file for the Sphinx documentation builder.
@@ -39,7 +40,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'm2r'
+        "m2r2",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -75,25 +76,56 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_material"
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
+if html_theme == "sphinx_material":
+    html_theme_options = {
+        "google_analytics_account": "",
+        "base_url": "https://docs.seldon.io/",
+        "color_primary": "indigo",
+        "color_accent": "teal",
+        "repo_url": "https://github.com/SeldonIO/seldon-core",
+        "repo_name": "Seldon Core",
+        "nav_title": "Seldon Open Source Projects",
+        "globaltoc_depth": 4,
+        "globaltoc_collapse": True,
+        "globaltoc_includehidden": False,
+        "repo_type": "github",
+        "nav_links": [
+            {
+                "href": "https://github.com/SeldonIO/seldon-core",
+                "internal": False,
+                "title": "Seldon Core",
+            },
+            {
+                "href": "https://github.com/SeldonIO/alibi",
+                "internal": False,
+                "title": "Alibi Explain",
+            },
+            {
+                "href": "https://github.com/SeldonIO/alibi-detect",
+                "internal": False,
+                "title": "Alibi Detect",
+            },
+            {
+                "href": "https://github.com/SeldonIO/mlserver",
+                "internal": False,
+                "title": "MLServer",
+            },
+            {
+                "href": "https://github.com/SeldonIO/tempo",
+                "internal": False,
+                "title": "Tempo SDK",
+            },
+        ],
+    }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+    extensions.append("sphinx_material")
+    html_theme_path = sphinx_material.html_theme_path()
+    html_context = sphinx_material.get_html_context()
 
-# override default theme width
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-    ],
-}
+html_sidebars = {"**": ["logo-text.html", "globaltoc.html", "localtoc.html"]}
+
 
 
 # Custom sidebar templates, must be a dictionary that maps document names
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -7,39 +7,55 @@
    :alt: Seldon logo
    :align: center
 
-Seldon
-------
-
 Seldon provides a set of tools for deploying machine learning models at scale.
 
- - Deploy machine learning models in the cloud or on-premise.
- - Get metrics and ensure proper governance and compliance for your running machine learning models
+- Deploy machine learning models in the cloud or on-premise.
+- Get metrics and ensure proper governance and compliance for your running machine learning models
 
+|
 
 Projects
 --------
 
-  - Seldon Core machine learning deployment on Kubernetes
-    
-    - `Seldon Core on GitHub`_
-    - `Seldon Core Technical Documentation`_
-    
-  - alibi: Algorithms for monitoring and explaining machine learning models
-  
-    - `alibi on Github`_
-    - `alibi Documentation`_
+Seldon Core machine learning deployment on Kubernetes
 
-  - alibi-detect: Algorithms for outlier detection, concept drift and metrics
-  
-    - `alibi-detect on Github`_
-    - `alibi-detect Documentation`_
-    
+- `Seldon Core on GitHub`_
+- `Seldon Core Technical Documentation`_
+
+Alibi Explain: Algorithms for AI Explainability for machine learning models
+
+- `alibi on Github`_
+- `alibi Documentation`_
+
+Alibi Detect: Algorithms for outlier detection, concept drift and metrics
+
+- `alibi-detect on Github`_
+- `alibi-detect Documentation`_
+
+Tempo: MLOps SDK for accelerating data science experimentation with Seldon Core and KFserving
+
+- `Tempo on Github`_
+- `Tempo Documentation`_
+
+MLServer: High performance ayncio machine learning model server for Seldon and KFserving
+
+- `MLServer on Github`_
+
+
+
+
 .. _Seldon Core on GitHub: https://github.com/SeldonIO/seldon-core
 .. _Seldon Core Technical Documentation: https://docs.seldon.io/projects/seldon-core/en/latest/    
 .. _alibi on Github: https://github.com/SeldonIO/alibi
 .. _alibi Documentation: https://docs.seldon.io/projects/alibi/en/latest/    
+
 .. _alibi-detect on Github: https://github.com/SeldonIO/alibi-detect
 .. _alibi-detect Documentation: https://docs.seldon.io/projects/alibi-detect/en/latest/    
+
+.. _Tempo on Github: https://github.com/SeldonIO/tempo
+.. _Tempo Documentation: https://tempo.readthedocs.io/en/latest/    
+
+.. _MLServer on Github: https://github.com/SeldonIO/MLServer
 
 .. toctree::
    :maxdepth: 2
